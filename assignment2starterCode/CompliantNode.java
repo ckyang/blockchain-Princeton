640778baa6509166b4cfd70f34cf16adc9a0d374
Clark Yang <ckyang0225@gmail.com>
2019-09-21 22:14:49 +0800

    Implement prototype

--- assignment2starterCode/CompliantNode.java
+++ assignment2starterCode/CompliantNode.java
@@ -3,30 +3,41 @@ import java.util.*;
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
 
+    private double m_graph, m_malicious, m_txDistribution;
+    private int m_numRounds;
+    private boolean[] m_followees;
+
+    private Set<Transaction> m_agree;
+
     // Setup the user input
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
-        // IMPLEMENT THIS
+        m_graph = p_graph;
+        m_malicious = p_malicious;
+        m_txDistribution = p_txDistribution;
+        m_numRounds = numRounds;
     }
 
     // Setup the followee -> follower graph
     public void setFollowees(boolean[] followees) {
-        // IMPLEMENT THIS
+        m_followees = followees;
     }
 
     // (1) Initial transaction status
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-        // IMPLEMENT THIS
+        m_agree = pendingTransactions;
     }
 
     // (2) For each round, it will be called to get the agreed transactions
     public Set<Transaction> sendToFollowers() {
-        // IMPLEMENT THIS
-        Set<Transaction> res = new HashSet<Transaction>();
-        return res;
+        return m_agree;
     }
 
     // (3) For each round, it will be called to update the latest transaction status
     public void receiveFromFollowees(Set<Candidate> candidates) {
-        // IMPLEMENT THIS
+        m_agree.clear();
+
+        for(Candidate c : candidates) {
+            m_agree.add(c.tx);
+        }
     }
 }
